daf21487275782d0222fb36ece6f2aa3f493668c
skohagen <skohage@gmail.com>
2020-02-17 21:16:27 -0500

    Initial Commit to Github

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+   <v-app>
+    <QuestionsAndAnswers/>
+   </v-app>
+  </div>
+</template>
+
+<script>
+import QuestionsAndAnswers from "./components/QuestionsAndAnswers"
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+export default {
+  name: "App",
+  components: {
+    QuestionsAndAnswers
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+.v-overlay:before{
+  
+}
+</style>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCa46TODhGYoNmxmeeViAXV3Re60cE1iDo",
+  authDomain: "mi-hpv.firebaseapp.com",
+  databaseURL: "https://mi-hpv.firebaseio.com",
+  projectId: "mi-hpv",
+  storageBucket: "mi-hpv.appspot.com",
+  messagingSenderId: "1017498781282"
+};
